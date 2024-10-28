486f4f1587eb0950eefbc1d42b5430ce6e5c1693
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-10-28 02:49:18 +0200

    Added input validation

--- scripts.js
+++ scripts.js
@@ -5,7 +5,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
 
-  // Additional validation
+  //Input validation
+  if (!dividend || !divider) {
+    result.innerText = "Error: Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+  result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
 });
