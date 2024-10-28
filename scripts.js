82a90172f7b93e652751a89240382776b5d7eb08
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-10-28 04:20:02 +0200

    Changed If structure

--- scripts.js
+++ scripts.js
@@ -12,13 +12,14 @@ form.addEventListener("submit", (event) => {
   if (!dividend || !divider) {
     result.innerText = "Error: Division not performed. Both values are required in inputs. Try again";
     return;
+  }
+
   //Zero Dividion Error handling
-  }else if(divider === 0){
+  if(divider === 0){
       result.innerText = "Error: Division by zero is not allowed. Please enter a non-zero value for the divisor.";
       console.trace()
       return;
-  
-  }else{
-  result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
   }
+    
+  result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
 });
