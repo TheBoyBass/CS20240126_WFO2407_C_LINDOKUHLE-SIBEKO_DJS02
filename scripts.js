00e12ca1a9b7c25dc796ab6f4a2cd02dc2f7fd26
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-10-28 02:15:24 +0200

    Made sure result is always a whole number

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider);
+  result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
 
   // Additional validation
 });
