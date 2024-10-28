55fa65188707927de509fa3d62aedb823f16755a
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-10-28 03:56:42 +0200

    Added Zero division Validation

--- scripts.js
+++ scripts.js
@@ -6,10 +6,19 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
+  
+
   //Input validation
   if (!dividend || !divider) {
     result.innerText = "Error: Division not performed. Both values are required in inputs. Try again";
     return;
-  }
+  //Zero Dividion Error handling
+  }else if(divider === 0){
+      result.innerText = "Error: Division by zero is not allowed. Please enter a non-zero value for the divisor.";
+      console.trace()
+      return;
+  
+  }else{
   result.innerText = Math.floor(dividend / divider); //Added math.floor to make sure the result is always rounded of to nearest interger
+  }
 });
